3fb921c69733f44cbef7f457de0e16dd1aed4dba
dani <danimohamadnejad@gmail.com>
2022-11-06 18:21:55 +0330

    ff

---
+++ framework/Application.php
@@ -0,0 +1,18 @@
+<?php
+namespace Framework;
+use Framework\Http\Request;
+class Application{
+  private static $instance = null;
+  private function __construct(){
+  } 
+  public static function instance(){
+    if(is_null(static::$instance)){
+        static::$instance = new Application();
+    }
+    return static::$instance;
+  } 
+  
+  public function request(){
+   return Request::instance(); 
+  }
+}
---
+++ framework/Http/Request.php
@@ -0,0 +1,16 @@
+<?php
+namespace Framework\Http;
+class Request{
+  private static $instance = null;
+  private function __construct(){
+  } 
+  public static function instance(){
+    if(is_null(static::$instance)){
+        static::$instance = new Request();
+    }
+    return static::$instance;
+  } 
+  public function uri(){
+    return $_SERVER['REQUEST_URI'];
+  }
+}
