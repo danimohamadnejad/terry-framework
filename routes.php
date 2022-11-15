623c9970e0a91ad98f0bd86c9efa3b9e6ddc1bb8
dani <danimohamadnejad@gmail.com>
2022-11-15 10:02:03 +0330

    x

--- routes.php
+++ routes.php
@@ -3,11 +3,11 @@ use Framework\Routing\Route;
 use App\Http\Controllers\HomeController;
 
 Route::group(['prefix'=>'site', 'alias'=>'site.'], function(){
-    Route::group(['prefix'=>'products.'], function(){
-        Route::group(['prefix'=>'amazon-products.'], function(){
-            Route::get("{name}", [HomeController::class, 'index']);
+    Route::group(['prefix'=>'products', 'alias'=>'products.'], function(){
+        Route::get("", [HomeController::class, 'products'])->name('products-index');
+        Route::group(['prefix'=>'amazon-products'], function(){
+            Route::get("{name}", [HomeController::class, 'index'])->name('amazon-products-index');
         }); 
     });
 });
- 
  
