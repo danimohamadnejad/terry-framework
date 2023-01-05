139a9942ec47c74267007da16466fd71c6b0ddbe
danial mohamadnejad <danimohamadnejad@gmail.com>
2023-01-05 22:26:54 +0330

    X

--- framework/Routing/Router.php
+++ framework/Routing/Router.php
@@ -23,37 +23,25 @@ class Router{
     $uri = Request::insance()->uri();
     $uri_sections = explode('/', $uri);
     $found_route = null;
-    /* now we compare request uri with uri of every single route and if match with one of them
-     we stop comparing other routes and return matched route*/
     foreach($routes as $route){
      $route_uri = $route->get_uri_pattern();
-     /* first we compare request uri and route uri equivalence. if they are equal, 
-     then we found it!!!*/
      if($uri == $route_uri){
        return $route;
      }
-     /* now that we are here, it is obvious that $uri and $route_uri are not equal exactly.  */
-     
-     /* when we get here, we know that our request must match a route which is parametric
-     which means one or more of it's sections contain  { and } */
- 
-     /* if number of sections in $uri is greater than number of sections in $route_uri, we can skip
-      current route and go to next one using continue statement */
      $route_segments = $route->get_segements();
      if(count($uri_sections) > count($route_segments)){
        continue;
      }
-     
-     /* the technique we use here to find route is that we suppose we found route so we say $found_route
-     equals current route */
-     
      $found_route = $route;
-     
-     /* then we try to disprove it. if we can disprove it we can skip current route and go to next
-     one and if we cant, then we just found it!!!!  */
-     
      foreach($route_segments as $index=>$segment){
-       
+       $segment_in_uri = isset($uri_sections[$index]) ? $uri_sections[$index] : null;
+       if(!$segment->is_parametric() && (is_null($segment_in_uri) || $segment_in_uri!==$segment->get_name())){
+        $found_route = null;
+        break;
+       }else if(($segment->is_parametric() && !$segment->is_optional()) && is_null($segment_in_uri)){
+        $found_route = null;
+        break;
+       }
      }
      if($found_route)
      {
--- routes.php
+++ routes.php
@@ -1,11 +1,7 @@
 <?php
 use Framework\Routing\Route;
 use App\Http\Controllers\HomeController;
-
-/* these routes are not parametric at all if a request is to match one of them, 
-request uri must equal route uri */
 Route::get("test/users", [HomeController::class, "show_users"]);
 Route::get("test/news", [HomeController::class, "show_news"]);
-/*  */
 Route::get('test/users/{id}', [HomeController::class, 'show_user']);
 Route::get('test/reports/{year?}', [HomeController::class, 'show_reports']);
