d8ff81c3252ccc6e5f280bf67207dbc402b65aad
danial mohamadnejad <danimohamadnejad@gmail.com>
2023-01-05 21:47:30 +0330

    x

--- framework/Routing/Router.php
+++ framework/Routing/Router.php
@@ -14,20 +14,23 @@ class Router{
     }
     return static::$instance;
   } 
-  
   public function find_route(){
-   $routes = Route::get_routes_by_method(Request::instance()->get_method());
-   if(empty($routes)){
+    $routes = Route::get_routes_by_method(Request::instance()->get_method());
+    if(empty($routes)){
      throw new \Exception("No route found");
-   }
-   $uri = Request::instance()->uri();
-   $uri_sections = explode("/", $uri);
-   foreach($routes as $route){
+    }
+    $uri = Request::insance()->uri();
+    $uri_sections = explode('/', $uri);
+    $found_route = null;
+    /* now we compare request uri with uri of every single route and if match with one of them
+     we stop comparing other routes and return matched route*/
+    foreach($routes as $route){
      $route_uri = $route->get_uri_pattern();
-     if($route_uri === $uri){
-      return $route;
+     /* first we compare request uri and route uri equivalence. if they are equal, 
+     then we found it!!!*/
+     if($uri == $route_uri){
+       return $route;
      }
-     
    }
   }
 }
