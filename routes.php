d97b66cc36f76b8b4e8420605a53c9d9c12cdb72
danial mohamadnejad <danimohamadnejad@gmail.com>
2023-01-05 22:30:34 +0330

    X

--- routes.php
+++ routes.php
@@ -3,5 +3,3 @@ use Framework\Routing\Route;
 use App\Http\Controllers\HomeController;
 Route::get("users", [HomeController::class, "show_users"]);
 Route::get("news", [HomeController::class, "show_news"]);
-Route::get('users/{id}', [HomeController::class, 'show_user']);
-Route::get('reports/{year?}', [HomeController::class, 'show_reports']);
