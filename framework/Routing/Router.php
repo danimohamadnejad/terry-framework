defc8230368682b592f951d6d49544382a6939f3
danial mohamadnejad <danimohamadnejad@gmail.com>
2023-01-05 22:34:25 +0330

    X

--- framework/Routing/Router.php
+++ framework/Routing/Router.php
@@ -18,35 +18,36 @@ class Router{
   public function find_route(){
     $routes = Route::get_routes_by_method(Request::instance()->get_method());
     if(empty($routes)){
-      throw new \Exception("No route found");
+     throw new \Exception("No route found");
     }
-    $uri = Request::instance()->uri();
-    $uri_sections = explode("/", $uri);
+    $uri = Request::insance()->uri();
+    $uri_sections = explode('/', $uri);
     $found_route = null;
     foreach($routes as $route){
-      $route_uri = $route->get_uri_pattern();
-      if($route_uri === $uri){
+     $route_uri = $route->get_uri_pattern();
+     if($uri == $route_uri){
        return $route;
-      }
-      $route_segments = $route->get_segments();
-      if(count($uri_sections) > count($route_segments)){
+     }
+     $route_segments = $route->get_segements();
+     if(count($uri_sections) > count($route_segments)){
        continue;
-      }
-      $found_route = clone $route;
-      foreach($route_segments as $index=>$segment){
-        $segment_in_uri = isset($uri_sections[$index]) ? $uri_sections[$index] : null;
-        if(!$segment->is_parametric() && (is_null($segment_in_uri) || $segment_in_uri!==$segment->get_name())){
-         $found_route = null;
-         break;
-        }else if(($segment->is_parametric() && !$segment->is_optional()) && is_null($segment_in_uri)){
-         $found_route = null;
-         break;
-        }
-      }
-      if($found_route)
-       return $found_route;
+     }
+     $found_route = $route;
+     foreach($route_segments as $index=>$segment){
+       $segment_in_uri = isset($uri_sections[$index]) ? $uri_sections[$index] : null;
+       if(!$segment->is_parametric() && (is_null($segment_in_uri) || $segment_in_uri!==$segment->get_name())){
+        $found_route = null;
+        break;
+       }else if(($segment->is_parametric() && !$segment->is_optional()) && is_null($segment_in_uri)){
+        $found_route = null;
+        break;
+       }
+     }
+     if($found_route)
+     {
+       break;
+     }
     }
-    return null;
+    return $found_route;
    }
-
 }
