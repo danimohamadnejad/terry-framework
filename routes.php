ca78333d34214c4507706dc99d991545b622f276
danial mohamadnejad <danimohamadnejad@gmail.com>
2023-03-13 00:31:33 +0330

    X

--- routes.php
+++ routes.php
@@ -1,10 +1,23 @@
 <?php
 use Framework\Routing\Route;
 use App\Http\Controllers\HomeController;
-Route::get("", [HomeController::class,'home']);
-Route::group(['prefix'=>'site'], function(){
-  Route::get("{name}", [HomeController::class, 'site_with_name']);
-  Route::group(['prefix'=>'friends'], function(){
-    Route::get("", [HomeController::class, 'friends']);
-  });  
+Route::group(['prefix'=>'products',], function(){ 
+    Route::get('', [HomeController::class, 'show_products']);  /* /products */
+    Route::group(['prefix'=>'{id}'], function(){                
+     Route::get('', [HomeController::class, 'show_product']);   /* /products/3 */
+    });
 });
+Route::get('index', [HomeController::class, 'show_home']);     /* /index */
+Route::group(['prefix'=>'users'], function(){                  
+    Route::get('', [HomeController::class, 'show_users']);     /* /users */
+    Route::group(['prefix'=>'{id}'], function(){               
+        Route::get('', [HomeController::class, 'show_user']);  /* /users/13 */ 
+        Route::group(['prefix'=>'profile'], function(){
+            Route::get('', [HomeController::class, 'show_user_profile']); /* /users/13/profile */ 
+        });
+    });
+});
+
+/* 
+Calling group methods on users: [ ['prefix'=>'users'], ['prefix'=>'{id'}], ['prefix'=>'profile'] ]
+*/
