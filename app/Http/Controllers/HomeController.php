edd3120f8575100c98e49a0d9599e5eaeb76aef9
danial mohamadnejad <danimohamadnejad@gmail.com>
2023-03-13 00:31:51 +0330

    X

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,12 +1,22 @@
 <?php
 namespace App\Http\Controllers;
 class HomeController{
-    public function index(){
+    public function show_products(){
+        die('products');
     }
-    public function home(){
-
+    public function show_product($id){
+        die('product: '.$id);
     }
-    public function site_with_name(){
-
+    public function show_users(){
+        die('users');
+    }
+    public function show_user($id){
+        die("user: ".$id);
+    }
+    public function show_user_profile($id){
+        die("profile of user with id: ".$id);
+    }
+    public function show_home(){
+        die('this is home');
     }
 }
