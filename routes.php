75256d1b2d55664fc2c8c9420a91af15b4565ea9
danial mohamadnejad <danimohamadnejad@gmail.com>
2023-03-13 00:49:18 +0330

    X

--- routes.php
+++ routes.php
@@ -1,19 +1,19 @@
 <?php
 use Framework\Routing\Route;
 use App\Http\Controllers\HomeController;
-Route::group(['prefix'=>'products',], function(){ 
-    Route::get('', [HomeController::class, 'show_products']);  /* /products */
+Route::group(['prefix'=>'products', 'alias'=>'products.'], function(){ 
+    Route::get('', [HomeController::class, 'show_products'])->name('search');  /* /products */
     Route::group(['prefix'=>'{id}'], function(){                
-     Route::get('', [HomeController::class, 'show_product']);   /* /products/3 */
+     Route::get('', [HomeController::class, 'show_product'])->name('show');   /* /products/3 */
     });
 });
 Route::get('index', [HomeController::class, 'show_home']);     /* /index */
-Route::group(['prefix'=>'users'], function(){                  
-    Route::get('', [HomeController::class, 'show_users']);     /* /users */
+Route::group(['prefix'=>'users', 'alias'=>'users.'], function(){                  
+    Route::get('', [HomeController::class, 'show_users'])->name('search');     /* /users */
     Route::group(['prefix'=>'{id}'], function(){               
-        Route::get('', [HomeController::class, 'show_user']);  /* /users/13 */ 
+        Route::get('', [HomeController::class, 'show_user'])->name('show');  /* /users/13 */ 
         Route::group(['prefix'=>'profile'], function(){
-            Route::get('', [HomeController::class, 'show_user_profile']); /* /users/13/profile */ 
+            Route::get('', [HomeController::class, 'show_user_profile'])->name('profile'); /* /users/13/profile */ 
         });
     });
 });
