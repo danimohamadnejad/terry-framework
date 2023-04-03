26976e3b404d2cdb267430173e5129a75a68b91f
danial mohamadnejad <danimohamadnejad@gmail.com>
2023-04-04 03:26:11 +0430

    X

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,4 +3,16 @@ namespace App\Http\Controllers;
 class HomeController{
     public function index(){
     }
+    public function show_users(){
+        die('show users');
+    }
+    public function show_user(){
+        die('show user');
+    }
+    public function show_news(){
+        die('show news');
+    }
+    public function show_reports($year = null){
+        die('show reports of '.$year);
+    }
 }
