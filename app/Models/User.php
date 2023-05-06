0a4b89f84868bd669c9d439b0e7bddbce58d6a26
danial mohamadnejad <danimohamadnejad@gmail.com>
2023-05-07 02:32:31 +0430

    x

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,11 +1,15 @@
 <?php
 namespace App\models;
+use App\Models\Address;
+
 class User{
     public $name;
     public $age;
-    
-    public function __construct($name, $age){
+    public Address $address;
+
+    public function __construct($name, $age, Address $address){
         $this->name = $name;
         $this->age = $age;
+        $this->address = $address;
     }
 }
