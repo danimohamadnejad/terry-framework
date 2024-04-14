b070ec5af3ac118e19e11a6d175a7aba405a309d
danial mohamadnejad <danimohamadnejad@gmail.com>
2024-04-15 02:18:00 +0330

    x

--- app/Http/Middleware/GoodPrintMiddleware.php
+++ app/Http/Middleware/GoodPrintMiddleware.php
@@ -2,8 +2,9 @@
 namespace App\Http\Middleware;
 use Framework\Http\Request;
 
-class NicePrintMiddleware {
+class GoodPrintMiddleware {
   public function handle(Request $req, \Closure $next){
+   echo "printing good...<br/>"; 
    return $next($req); 
   }  
 }
--- app/Http/Middleware/NicePrintMiddleware.php
+++ app/Http/Middleware/NicePrintMiddleware.php
@@ -2,8 +2,9 @@
 namespace App\Http\Middleware;
 use Framework\Http\Request;
 
-class GoodPrintMiddleware {
+class NicePrintMiddleware {
   public function handle(Request $req, \Closure $next){
+   echo "printing nice...<br/>"; 
    return $next($req); 
   }  
 }
